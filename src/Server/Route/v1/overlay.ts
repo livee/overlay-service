51157b9edd828466ab6dadc27a19330ebc5a2137
Eduard Bondarenko <eduardbcom@gmail.com>
2021-05-15 20:52:13 +0300

    Accept requests params

--- src/Server/Route/v1/overlay.ts
+++ src/Server/Route/v1/overlay.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as _ from 'lodash';
 
 import { LoggerWithContext } from '@src/Logger/LoggerWithContext';
 import { OverlayService } from '@src/Service/OverlayService';
@@ -9,8 +10,18 @@ function overlay(logger: LoggerWithContext, overlayService: OverlayService) {
     router.post(`/overlay`, async (req: express.Request, res: express.Response) => {
         const log = logger.create(`POST /overlay`);
 
+        const { url, corrId } = req.body;
+
+        if (!_.isString(url) || _.isEmpty(url)) {
+            return res.status(400).json({ code: 1001, message: 'Invalid request params' });
+        }
+
+        if (!_.isString(corrId) || _.isEmpty(corrId)) {
+            return res.status(400).json({ code: 1001, message: 'Invalid request params' });
+        }
+
         try {
-            await overlayService.run({ url: 'https://meet.staging.livee.com:3001/', corrId: '1' });
+            await overlayService.run({ url, corrId });
 
             return res.status(200).json({ code: 0 });
         } catch (error) {
@@ -23,8 +34,14 @@ function overlay(logger: LoggerWithContext, overlayService: OverlayService) {
     router.delete(`/overlay`, async (req: express.Request, res: express.Response) => {
         const log = logger.create(`POST /overlay`);
 
+        const { corrId } = req.body;
+
+        if (!_.isString(corrId) || _.isEmpty(corrId)) {
+            return res.status(400).json({ code: 1001, message: 'Invalid request params' });
+        }
+
         try {
-            await overlayService.stop({ corrId: '1' });
+            await overlayService.stop({ corrId });
 
             return res.status(200).json({ code: 0 });
         } catch (error) {
